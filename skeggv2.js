347c8b1b3cb59fe088b60df7275cb01cf19805f3
LiameboAD <agustinkaren06@gmail.com>
2026-05-31 12:50:55 +0800

    Fix formatting issues in skeggv2.js

--- skeggv2.js
+++ skeggv2.js
@@ -1,5 +1,9 @@
+// More info about enabling, testing, and submitting mods: https://R74ncom.github.io/InfiniteChef-Mods/
+
+// Add a basic ingredient using the 'egg' ingredient as a base
+
 addIngredient("skegg",{
     name:"Skegg",
-    type:"egg",
-    color:"#9AB1FF"
+    type:"egg", //will inherit all properties of the "egg" ingredient
+    color:"#9AB1FF" //sky blue
 })
